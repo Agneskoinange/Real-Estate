cfed4129284369a6094c746ef56ca0296feb9432
Agnes <koinageagness@gmail.com>
2022-11-23 11:48:02 +0300

    ft:create page structure

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = () => (
+  
+)
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Real Estate Application</h1>
+      
+    </div>
+  )
+}
