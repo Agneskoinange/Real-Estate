39776b1e85cc1d48ab4150f5776449aae5d6f107
Agnes <koinageagness@gmail.com>
2022-12-13 13:18:05 +0300

    ft: add  property details

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import Head from "next/head";
+// import NProgresss from "ngprogress";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Layout from "../components/Layout"
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import { baseUrl, fetchApi } from '../utils/fetchApi';
+import Property from "../components/Property";
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
 
@@ -38,6 +39,7 @@ export default function Home({ propertiesForSale, propertiesForRent}) {
       <Flex flexWrap="wrap">
         
       </Flex>
+      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
 
       <Banner
         purpose="BUY A HOME"
@@ -50,6 +52,7 @@ export default function Home({ propertiesForSale, propertiesForRent}) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
       
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Box>
   )
 }
